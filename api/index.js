59bdd31a86c1b541178357af58afa74605d6f0b2
Gary2303 <84616105+Gary2303@users.noreply.github.com>
2021-07-13 14:46:48 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -31,13 +31,13 @@ bot.onText(/\/predict/, (msg) => {
 bot.on('message',(msg) => { 
     if(state == 1){
         console.log(msg.text);
-        s = msg.text.split ("|");
+        s = msg.text.split ('|');
         i = s[0]
         v = s[1]
         model.predict(
          [
-            parsefloat(s[0]), // string to float
-            parsefloat(s[1])
+            parseFloat(s[0]), // string to float
+            parseFloat(s[1])
          ]
     ).then((jres)=>{
        console.log(jres);      
